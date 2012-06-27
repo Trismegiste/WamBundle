ee5538a50ad3e5cd951d8e335b04c06c36679e1d
Trismegiste <florent.g@allopneus.com>
2012-06-27 22:52:50 +0200

    code reader

---
+++ CodeReader.php
@@ -0,0 +1,70 @@
+<?php
+
+/* * ****************************************************************************
+ * Warren's Abstract Machine  -  Implementation by Stefan Buettcher
+ *
+ * developed:   December 2001 until February 2002
+ *
+ * CodeReader.java contains the CodeReader class that transforms a WAM code
+ * input file into a Program structure (cf. Program.java / Statement.java)
+ * **************************************************************************** */
+
+class CodeReader {
+
+    public function __construct() {
+        
+    }
+
+    /**
+     * @return Program
+     */
+    public static function readProgram($fileName) {
+        // BufferedReader b;
+        try {
+            $b = fopen($fileName, 'r');
+            $p = new Program();
+            // Statement s;
+            // String str;
+            do {
+                $str = fgets($b);
+                if ($str != null) {
+                    //int j;
+                    $str = trim($str);
+                    if (strlen($str) == 0)
+                        continue;
+                    if (in_array($str[0], array(';', '#', '%')))
+                        continue;
+                    $mark = "";
+                    $j = strpos($str, ":");
+                    if ($j > 0) {
+                        $mark = trim(substr($str, 0, $j));
+                        $str = trim(substr($str, $j + 1));
+                    }
+                    $fonction = "";
+                    $j = strpos($str, " ");
+                    if ($j > 0) {
+                        $fonction = trim(substr($str, 0, $j));
+                        $str = trim(substr($str, $j + 1));
+                        $j = strpos($str, " ");
+                        if ($j > 0)
+                            $s = new Statement($mark, $fonction, trim(substr($str, 0, $j)), trim(substr($str, $j + 1)));
+                        else
+                            $s = new Statement($mark, $fonction, $str);
+                    }
+                    else
+                        $s = new Statement($mark, $str, "");
+                    $p->addStatement($s);
+                }
+            } while ($str != null);
+            fclose($b);
+            $p->updateLabels();
+            return $p;
+        } catch (Exception $io) {
+            return null;
+        }
+    }
+
+// end of CodeReader.readProgram()
+}
+
+// end of class CodeReader
