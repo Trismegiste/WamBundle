f7d0277a751551a960cef9e9c0774db816b65333
Florent <florent.g@allopneus.com>
2012-06-28 12:39:11 +0200

    program ok

--- Program.php
+++ Program.php
@@ -1,239 +1,281 @@
 <?php
-/******************************************************************************
+
+/* * ****************************************************************************
  * Warren's Abstract Machine  -  Implementation by Stefan Buettcher
  *
  * developed:   December 2001 until February 2002
  *
  * Program.java contains the WAM program management class Program. A Program
  * consists of an array of Statements (cf. Statement.java).
- ******************************************************************************/
+ * **************************************************************************** */
 
 // Program class manages WAM programs, consisting of list (vector) of statements
-class Program {
-  const callWrite         = -10;
-  const callWriteLn       = -11;
-  const callNewLine       = -12;
-  const callConsult       = -13;
-  const callReconsult     = -14;
-  const callLoad          = -15;
-  const callAssert        = -16;
-  const callRetractOne    = -17;
-  const callRetractAll    = -18;
-  const callIsInteger     = -19;
-  const callIsAtom        = -20;
-  const callIsBound       = -21;
-  const callReadLn        = -22;
-  const callCall          = -23;
+class Program
+{
 
-  private $statements = array();
-  public TreeMap labels;
-  public WAM owner;
+    const callWrite = -10;
+    const callWriteLn = -11;
+    const callNewLine = -12;
+    const callConsult = -13;
+    const callReconsult = -14;
+    const callLoad = -15;
+    const callAssert = -16;
+    const callRetractOne = -17;
+    const callRetractAll = -18;
+    const callIsInteger = -19;
+    const callIsAtom = -20;
+    const callIsBound = -21;
+    const callReadLn = -22;
+    const callCall = -23;
 
-  public Program() {
-    statements = new Vector();
-    owner = null;
-    labels = new TreeMap();
-  } // end of Program.Program()
+    private $statements = array();
+    public $labels = array();
+    public $owner = null;  // WAM
 
-  public Program(WAM anOwner) {
-    statements = new Vector();
-    owner = anOwner;
-    labels = new TreeMap();
-  } // end of Program.Program(WAM)
+    public function __construct(WAM $anOwner = null)
+    {
+        $this->owner = $anOwner;
+    }
 
-  public void addProgram(Program p) {
-    if (p == null) return;
-    int cnt = p.getStatementCount();
-    boolean canAdd = true;
-    for (int i = 0; i < cnt; i++) {
-      Statement s = p.getStatement(i);
-      String label = s.getLabel();
-      if (label.length() > 0) {
-        if (labels.containsKey(label)) {
-          canAdd = false;
-          owner.writeLn("Error: Multiple occurrence of label \"" + label + "\". Use reconsult.");
+// end of Program.Program(WAM)
+
+    public function addProgram(Program $p)
+    {
+        if ($p == null)
+            return;
+        $cnt = $p->getStatementCount();
+        $canAdd = true;
+        for ($i = 0; $i < $cnt; $i++) {
+            $s = $p->getStatement($i);
+            $label = $s->getLabel();
+            if (strlen($label) > 0) {
+                if (array_key_exists($label, $this->labels)) {
+                    $canAdd = false;
+                    $this->owner->writeLn("Error: Multiple occurrence of label \"$label\". Use reconsult.");
+                } else {
+                    $this->labels[$label] = count($this->statements);
+                    $canAdd = true;
+                }
+            }
+            if ($canAdd)
+                $this->addStatement($s);
         }
-        else {
-          labels.put(label, new Integer(statements.size()));
-          canAdd = true;
+    }
+
+// end of Program.addProgram(Program)
+
+    public function addStatement(Statement $s)
+    {
+        $this->statements[] = $s;
+    }
+
+// end of Program.addStatement(Statement)
+
+    public function addStatementAtPosition(Statement $s, $position)
+    {
+        $tmp = array_splice($this->statements, $position);
+        $this->statements[] = $s;
+        $this->statements = array_merge($this->statements, $tmp);
+    }
+
+// end of Program.addStatementAtPosition(Statement, int)
+
+    public function getStatementCount()
+    {
+        return count($this->statements);
+    }
+
+// end of Program.getStatementCount()
+
+    public function getStatement($i)
+    {
+        return $this->statements[$i];
+    }
+
+// end of Program.getStatement(int)
+
+    public function deleteFromLine($lineNumber)
+    {
+        $result = 1;
+        if ($lineNumber >= 0) {
+            unset($this->statements[$lineNumber]);
+            while ((count($this->statements) > $lineNumber) && (strlen($this->statements[$lineNumber]->getLabel()) == 0)) {
+                $result++;
+                unset($this->statements[$lineNumber]);
+            }
+            $this->updateLabels();
         }
-      }
-      if (canAdd) addStatement(s);
+        return $result;
     }
-  } // end of Program.addProgram(Program)
 
-  public void addStatement(Statement s) {
-    statements.addElement(s);
-  } // end of Program.addStatement(Statement)
+// end of Program.deleteFromLine(int)
 
-  public void addStatementAtPosition(Statement s, int position) {
-    statements.insertElementAt(s, position);
-  } // end of Program.addStatementAtPosition(Statement, int)
-
-  public int getStatementCount() {
-    return statements.size();
-  } // end of Program.getStatementCount()
-
-  public Statement getStatement(int i) {
-    return (Statement)statements.elementAt(i);
-  } // end of Program.getStatement(int)
-
-  public int deleteFromLine(int lineNumber) {
-    int result = 1;
-    if (lineNumber >= 0) {
-      statements.removeElementAt(lineNumber);
-      while ((statements.size() > lineNumber) && (((Statement)statements.elementAt(lineNumber)).getLabel().length() == 0)) {
-        result++;
-        statements.removeElementAt(lineNumber);
-      }
-      updateLabels();
+    public function deleteFrom($label)
+    {
+        return $this->deleteFromLine($this->getLabelIndex($label));
     }
-    return result;
-  } // end of Program.deleteFromLine(int)
 
-  public int deleteFrom(String label) {
-    return deleteFromLine(getLabelIndex(label));
-  } // end of Program.deleteFrom(String)
+// end of Program.deleteFrom(String)
 
-  public int getLastClauseOf(String procedureName) {
-    int line = getLabelIndex(procedureName);
-    if (line >= 0) {
-      boolean finished = false;
-      Statement s;
-      do {
-        s = getStatement(line);
-        if ((s.operator == s.opTryMeElse) || (s.operator == s.opRetryMeElse))
-          line = s.jump;
-        else
-          finished = true;
-      } while (!finished);
-    }
-    return line;
-  } // end of Program.getLastClauseOfProcedure(String)
-
-  public int getLastClauseButOneOf(String procedureName) {
-    int result = -1;
-    int line = getLabelIndex(procedureName);
-    if (line >= 0) {
-      boolean finished = false;
-      Statement s;
-      do {
-        s = getStatement(line);
-        if ((s.operator == s.opTryMeElse) || (s.operator == s.opRetryMeElse)) {
-          result = line;
-          line = s.jump;
+    public function getLastClauseOf($procedureName)
+    {
+        $line = $this->getLabelIndex($procedureName);
+        if ($line >= 0) {
+            $finished = false;
+            //Statement s;
+            do {
+                $s = $this->getStatement($line);
+                if (($s->operator == Statement::opTryMeElse) || ($s->operator == Statement::opRetryMeElse))
+                    $line = $s->jump;
+                else
+                    $finished = true;
+            } while (!$finished);
         }
-        else
-          finished = true;
-      } while (!finished);
+        return $line;
     }
-    return result;
-  } // end of Program.getLastClauseButOneOf(String)
 
-  public void addClause(String label, Program code) {
-    int line = getLastClauseOf(label);
-    if (line >= 0) {  // there already exists such a label: add via try_me_else
-      Statement s = getStatement(line);
-      int index = s.getLabel().indexOf('~');
-      try {
-        int i;
-        if (index > 0)
-          i = Integer.parseInt(s.getLabel().substring(index + 1)) + 1;
-        else
-          i = 2;
-        // update the just-compiled program
-        String newLabel = label + "~" + i;
-        code.getStatement(0).setLabel(newLabel);
-        // update the previous clause: trust_me -> try_me_else
-        s.setFunction("try_me_else");
-        s.getArgs().setElementAt(newLabel, 0);
-        s.arg1 = newLabel;
-        s.setJump(statements.size());
-        // update labels and program itself
-        addProgram(code);
-      } catch (Exception e) { }
-    }
-    else {  // first label of that kind: just add to code and update jumpings
-      addProgram(code);
-      updateLabels();
-    }
-  }
+// end of Program.getLastClauseOfProcedure(String)
 
-  public int getLabelIndex(String label) {
-    for (int i = 0; i < statements.size(); i++)
-      if (label.compareTo(((Statement)statements.elementAt(i)).getLabel()) == 0)
-        return i;
-    return -1;
-  } // end of Program.getLabelIndex(String)
-
-  // updateLabels converts String label names in call, try_me_else and retry_me_else statements
-  // to integer values. internal predicates (e.g. write, consult) are transformed to negative line numbers
-  public void updateLabels() {
-    labels = new TreeMap();
-    String label;
-    int cnt = statements.size();
-    for (int i = 0; i < cnt; i++) {
-      Statement s = (Statement)statements.elementAt(i);
-      label = s.getLabel();
-      if (label.length() > 0)
-        labels.put(label, new Integer(i));
+    public function getLastClauseButOneOf($procedureName)
+    {
+        $result = -1;
+        $line = $this->getLabelIndex($procedureName);
+        if ($line >= 0) {
+            $finished = false;
+            //Statement s;
+            do {
+                $s = $this->getStatement($line);
+                if (($s->operator == Statement::opTryMeElse) || ($s->operator == Statement::opRetryMeElse)) {
+                    $result = $line;
+                    $line = $s->jump;
+                }
+                else
+                    $finished = true;
+            } while (!$finished);
+        }
+        return $result;
     }
-    for (int i = 0; i < cnt; i++) {
-      Statement s = (Statement)statements.elementAt(i);
-      if ((s.getFunction().compareTo("call") == 0) ||
-          (s.getFunction().compareTo("not_call") == 0) ||
-          (s.getFunction().compareTo("try_me_else") == 0) ||
-          (s.getFunction().compareTo("retry_me_else") == 0)) {
-        label = (String)(s.getArgs().elementAt(0));
-        s.setJump(-1);
-        if (labels.containsKey(label)) // label is a user-defined predicate
-          s.setJump(((Integer)labels.get(label)).intValue());
-        else {  // label is undefined or a built-in predicate
-          if (label.compareTo("atomic") == 0)
-            s.setJump(callIsAtom);
-          else if (label.compareTo("integer") == 0)
-            s.setJump(callIsInteger);
-          else if (label.compareTo("bound") == 0)
-            s.setJump(callIsBound);
-          else if (label.compareTo("write") == 0)
-            s.setJump(callWrite);
-          else if (label.compareTo("writeln") == 0)
-            s.setJump(callWriteLn);
-          else if (label.compareTo("call") == 0)
-            s.setJump(callCall);
-          else if ((label.compareTo("nl") == 0) || (label.compareTo("newline") == 0))
-            s.setJump(callNewLine);
-          else if (label.compareTo("consult") == 0)
-            s.setJump(callConsult);
-          else if (label.compareTo("reconsult") == 0)
-            s.setJump(callReconsult);
-          else if (label.compareTo("load") == 0)
-            s.setJump(callLoad);
-          else if ((label.compareTo("assert") == 0) || (label.compareTo("assertz") == 0))
-            s.setJump(callAssert);
-          else if ((label.compareTo("retract") == 0) || (label.compareTo("retractone") == 0))
-            s.setJump(callRetractOne);
-          else if (label.compareTo("retractall") == 0)
-            s.setJump(callRetractAll);
-          else if (label.compareTo("readln") == 0)
-            s.setJump(callReadLn);
-        } // end of (!labels.containsKey(label))
-      }
-    }
-  } // end of Program.updateLabels()
 
-  public String toString() {
-    String result = "";
-    for (int i = 0; i < statements.size(); i++) {
-      String line = "(";
-      if (i < 1000) line += "0";
-      if (i < 100) line += "0";
-      if (i < 10) line += "0";
-      line += i + ")  ";
-      result += line + ((Statement)statements.elementAt(i)).toString();
-      if (i < statements.size() - 1) result += "\n";
-    }
-    return result;
-  } // end of Program.toString()
+// end of Program.getLastClauseButOneOf(String)
 
-} // end of class Program
+    public function addClause($label, Program $code)
+    {
+        $line = $this->getLastClauseOf($label);
+        if ($line >= 0) {  // there already exists such a label: add via try_me_else
+            $s = $this->getStatement($line);
+            $index = strpos($s->getLabel(), '~');
+            try {
+                //int i;
+                if ($index > 0)
+                    $i = 1 + substr($s->getLabel(), $index + 1);
+                else
+                    $i = 2;
+                // update the just-compiled program
+                $newLabel = $label . "~" . $i;
+                $code->getStatement(0)->setLabel($newLabel);
+                // update the previous clause: trust_me -> try_me_else
+                $s->setFunction("try_me_else");
+                $tab = $s->getArgs();
+                $tab[$newLabel] = 0;
+                $s->arg1 = $newLabel;
+                $s->setJump(count($this->statements));
+                // update labels and program itself
+                $this->addProgram($code);
+            } catch (Exception $e) {
+
+            }
+        } else {  // first label of that kind: just add to code and update jumpings
+            $this->addProgram($code);
+            $this->updateLabels();
+        }
+    }
+
+    public function getLabelIndex($label)
+    {
+        foreach ($this->statements as $i => $item)
+            if ($label === $item->getLabel())
+                return $i;
+        return -1;
+    }
+
+// end of Program.getLabelIndex(String)
+    // updateLabels converts String label names in call, try_me_else and retry_me_else statements
+    // to integer values. internal predicates (e.g. write, consult) are transformed to negative line numbers
+    public function updateLabels()
+    {
+        $this->labels = array();
+        //String label;
+        $cnt = count($this->statements);
+        for ($i = 0; $i < $cnt; $i++) {
+            $s = $this->statements[$i];
+            $label = $s->getLabel();
+            if (strlen($label) > 0)
+                $this->labels[$label] = $i;
+        }
+        for ($i = 0; $i < $cnt; $i++) {
+            $s = $this->statements[$i];
+            if (in_array($s->getFunction(), array("call", "not_call", "try_me_else", "retry_me_else"))) {
+                $label = $s->getArgs();
+                $label = $label[0];
+                $s->setJump(-1);
+                if (array_key_exists($label, $this->labels)) // label is a user-defined predicate
+                    $s->setJump($this->labels[$label]);
+                else {  // label is undefined or a built-in predicate
+                    if ($label === "atomic")
+                        $s->setJump(self::callIsAtom);
+                    else if ($label === "integer")
+                        $s->setJump(self::callIsInteger);
+                    else if ($label === "bound")
+                        $s->setJump(self::callIsBound);
+                    else if ($label === "write")
+                        $s->setJump(self::callWrite);
+                    else if ($label === "writeln")
+                        $s->setJump(self::callWriteLn);
+                    else if ($label === "call")
+                        $s->setJump(self::callCall);
+                    else if (in_array($label, array("nl", "newline")))
+                        $s->setJump(self::callNewLine);
+                    else if ($label === "consult")
+                        $s->setJump(self::callConsult);
+                    else if ($label === "reconsult")
+                        $s->setJump(self::callReconsult);
+                    else if ($label === "load")
+                        $s->setJump(self::callLoad);
+                    else if (in_array($label, array("assert", "assertz")))
+                        $s->setJump(self::callAssert);
+                    else if (in_array($label, array("retract", "retractone")))
+                        $s->setJump(self::callRetractOne);
+                    else if ($label === "retractall")
+                        $s->setJump(self::callRetractAll);
+                    else if ($label === "readln")
+                        $s->setJump(self::callReadLn);
+                } // end of (!labels.containsKey(label))
+            }
+        }
+    }
+
+// end of Program.updateLabels()
+
+    public function __toString()
+    {
+        $result = "";
+        foreach ($this->statements as $st) {
+            $line = "(";
+            if ($i < 1000)
+                $line .= "0";
+            if ($i < 100)
+                $line .= "0";
+            if ($i < 10)
+                $line .= "0";
+            $line .= $i + ")  ";
+            $result .= $line . $st;
+            if ($i < (count($this->statements) - 1))
+                $result += "\n";
+        }
+        return $result;
+    }
+
+// end of Program.toString()
+}
+
+// end of class Program
