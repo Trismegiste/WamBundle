5a08de7ca05f2b5947a74185982157f2b271fc55
Florent <florent.g@allopneus.com>
2012-06-28 13:50:10 +0200

    win => wim

--- CodeReaderTest.php
+++ CodeReaderTest.php
@@ -5,6 +5,6 @@ require_once('Statement.php');
 require_once('CodeReader.php');
 
 $cr = new CodeReader();
-$p = $cr->readProgram('fixtures_test.win');
+$p = $cr->readProgram('fixtures_test.wim');
 echo $p;
 ?>
