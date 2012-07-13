579ce501f7e2d6d9d3db913f345fd4f30ccb32d6
Florent <florent.g@allopneus.com>
2012-07-13 17:22:20 +0200

    autoload for the win

--- CodeReaderTest.php
+++ CodeReaderTest.php
@@ -1,21 +1,14 @@
 <?php
 
+spl_autoload_register(function ($class) {
+            include_once $class . '.php';
+        });
+
 /**
  * Unit test for solver : does some queries and checking no regression
  *
  * @author flo
  */
-require_once('Program.php');
-require_once('Statement.php');
-require_once('CodeReader.php');
-require_once('CompilerStructure.php');
-require_once('Compiler.php');
-require_once('QueryCompiler.php');
-require_once('PrologCompiler.php');
-require_once('InnerClass.php');
-require_once('PrologContext.php');
-require_once('WAM.php');
-
 class CodeReaderTest extends PHPUnit_Framework_TestCase
 {
 
