e3c478200cdec2c2de0dd395419e2cdae77f0c11
Trismegiste <florent.g@allopneus.com>
2012-07-14 02:18:23 +0200

    rationalize new fixtures for test

--- WAMServiceTest.php
+++ WAMServiceTest.php
@@ -15,11 +15,11 @@ class WAMServiceTest extends PHPUnit_Framework_TestCase
         $solve = $wam->runQuery("consult('fixtures1.pro').");
         $this->assertCount(1, $solve);
         $this->assertTrue($solve[0]->succeed);
-        
+
         $solve = $wam->runQuery("equal(luke, luke).");
         $this->assertCount(1, $solve);
         $this->assertTrue($solve[0]->succeed);
-        
+
         $solve = $wam->runQuery("grandmere(X, luke).");
         $this->assertCount(3, $solve);
         $this->assertTrue($solve[0]->succeed);
@@ -35,12 +35,21 @@ class WAMServiceTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($solve[1]->succeed);
         $this->assertEquals('leia', $solve[1]->variable['X']);
         $this->assertFalse($solve[2]->succeed);
-        
+
         $solve = $wam->runQuery("frere(luke, leia).");
         $this->assertCount(3, $solve);
         $this->assertTrue($solve[0]->succeed);
         $this->assertTrue($solve[1]->succeed);
         $this->assertFalse($solve[2]->succeed);
+    }
+
+    public function testFixtures2()
+    {
+        $wam = new WAMService(new Program());
+
+        $solve = $wam->runQuery("consult('fixtures2.pro').");
+        $this->assertCount(1, $solve);
+        $this->assertTrue($solve[0]->succeed);
     }
 
 }
