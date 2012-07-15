a4c11fb43e518bff6003a50f6017876428ea46da
Trismegiste <florent.g@allopneus.com>
2012-07-15 10:47:28 +0200

    extending the prototype testcase

--- WAMServiceTest.php
+++ WAMServiceTest.php
@@ -5,31 +5,8 @@
  *
  * @author flo
  */
-class WAMServiceTest extends PHPUnit_Framework_TestCase
+class WAMServiceTest extends WAM_TestCase
 {
-
-    protected function checkSuccess($solve)
-    {
-        $this->assertCount(1, $solve);
-        $this->assertTrue($solve[0]->succeed);
-    }
-
-    protected function checkOneValueSuccess($solve, $key, $value)
-    {
-        $this->checkOneSolutionSuccess($solve, array($key => $value));
-    }
-
-    protected function checkOneSolutionSuccess($solve, array $expected)
-    {
-        $this->assertCount(2, $solve);
-        $this->assertTrue($solve[0]->succeed);
-        $this->assertCount(count($expected), $solve[0]->variable);
-        foreach ($expected as $key => $value) {
-            $this->assertArrayHasKey($key, $solve[0]->variable);
-            $this->assertEquals($value, $solve[0]->variable[$key]);
-        }
-        $this->assertFalse($solve[1]->succeed);
-    }
 
     public function testFixtures1()
     {
