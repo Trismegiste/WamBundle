0f96c7c90ac972f0ac24212c57ed3b74b008b37f
Florent <florent.g@allopneus.com>
2012-07-17 12:13:46 +0200

    adding 8 queens functional test

---
+++ WAMEightQueensTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Test for WAMService : example of classical non deterministic problem
+ * for e-commerce
+ */
+class WAMEightQueensTest extends WAM_TestCase
+{
+
+    public function testFixtures()
+    {
+        $wam = new WAMService(new Program());
+
+        $solve = $wam->runQuery("consult('eightqueens.pro').");
+        $this->checkSuccess($solve);
+
+        return $wam;
+    }
+
+    /**
+     * @depends testFixtures
+     */
+    public function testAllSolutions(WAMService $wam)
+    {
+        $solve = $wam->runQuery("soluce(X), solution(X).");
+        $this->assertCount(93, $solve);
+        // there are 92 solutions to 8 queens problem
+        for ($k = 0; $k < 92; $k++) {
+            $row = $solve[$k];
+            $this->assertTrue($row->succeed);
+        }
+        // the last is false (because backtrack)
+        $this->assertFalse($solve[92]->succeed);
+    }
+
+}
