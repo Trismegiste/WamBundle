a1f6eecd9140ecafa9bcc008cc5f51f96c6253bc
Trismegiste <florent.g@allopneus.com>
2012-07-19 23:39:21 +0200

    new interface for output string in a buffer

---
+++ OutputInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Interface for output a string
+ * @author flo
+ */
+interface OutputInterface
+{
+
+    /**
+     * Close a line and start a new one
+     * @param string $str 
+     */
+    function writeLn($str);
+
+    /**
+     * Continue a line with a string
+     * @param string $str 
+     */
+    function write($str);
+}
