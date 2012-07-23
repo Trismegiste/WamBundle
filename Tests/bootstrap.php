155a064dbde8de681e463fa17b852b3e43664894
Florent <florent.g@allopneus.com>
2012-07-23 08:51:04 +0200

    die William die + colors

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -6,5 +6,5 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'WAM_TestCase.php';
 
 spl_autoload_register(function ($class) {
             preg_match('#([^\\\\]+)$#', $class, $ret);
-            require_once __DIR__ . '\..\\' . $ret[1] . '.php';
+            require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $ret[1] . '.php';
         });
