ad26fcb6c50e3ab25e42311be079f5462f02c8be
Florent <florent.g@allopneus.com>
2012-07-30 17:45:16 +0200

    removing debug msg

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,6 +8,5 @@ spl_autoload_register(function ($class) {
             $found = preg_match('#^Trismegiste\\\\WamBundle\\\\(.+)$#', $class, $ret);
             if (!$found) trigger_error ("Class $class not autoloaded.");
             $relPath = str_replace('\\', DIRECTORY_SEPARATOR, $ret[1]);
-            echo $relPath."\n";
             require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $relPath . '.php';
         });
