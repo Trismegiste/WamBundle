5c21ba33ddc126cb32c3793aa25cba3671a38176
Trismegiste <florent.g@allopneus.com>
2012-08-05 17:53:16 +0200

    Adding command for symfony 2

---
+++ Command/WAMCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Trismegiste\WamBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+use Trismegiste\WamBundle\Prolog\WAMConsole;
+
+/**
+ * Wrapper for WAMConsole
+ */
+class WAMCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this->
+                setDefinition(array())->
+                setDescription('Run the prolog console with a Warren Abstract Machine')->
+                setHelp("The <info>wam:prolog:console</info> command launches a console for Prolog.
+It embeds a compiler and a Warren Abstract Machine.
+By Stefan Büttcher and Florent Genette")->
+                setName('wam:prolog:console');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        WAMConsole::main(array());
+    }
+
+}
